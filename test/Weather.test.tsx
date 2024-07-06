6d9ff7a4b4be849c88b296b4f2ad34ef8c95d546
Lewis <lsm10100@gmail.com>
2024-07-06 21:04:22 +0100

    move tests to test dir

--- test/Weather.test.tsx
+++ test/Weather.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import * as React from "react";
 import '@testing-library/jest-dom'
-import Weather from './Weather';
+import Weather from '../src/Weather';
 
 
 /**
