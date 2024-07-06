9c89eb5c0629c27241931cd1b473cdb38d684d1f
Lewis <lsm10100@gmail.com>
2024-07-06 21:13:13 +0100

    use weather data as props

--- src/Weather.tsx
+++ src/Weather.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import {useEffect, useState} from "react";
 import styled from "styled-components";
+import { LocationWeatherData } from "./types/WeatherDataTypes";
 
 
 type WeatherProps =  {
-    test: string
+    locationWeatherData: LocationWeatherData
 }
 
 
