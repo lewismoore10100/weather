49524f7b187fba956ce87ce06467138ed6bd088c
lewis <lsm10100@gmail.com>
2024-07-12 22:05:30 +0100

    refactor display

--- src/WeatherDisplay.tsx
+++ src/WeatherDisplay.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import {useEffect, useState} from "react";
-import styled from "styled-components";
 import { LocationWeatherData } from "./types/WeatherDataTypes";
 import Location from "./Location";
 
